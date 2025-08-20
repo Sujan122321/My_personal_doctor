15d652c711772c18dfb4a4e3cbcc8be94083e5d3
Sujan122321 <suzanstha203@gmail.com>
2025-08-20 20:41:19 +0545

    requirements added

--- src/brain_of_the_doctor.py
+++ src/brain_of_the_doctor.py
@@ -37,7 +37,7 @@ messages = [
              },
             {
                 "type": "image_url",
-                "image_url": {"url": f"data:image/jpeg;base64,{encoded_image}"}
+                "image_url": {"url": f"data:image/jpeg;base64,{encoded_image}"}2
             },
         ],
     }
